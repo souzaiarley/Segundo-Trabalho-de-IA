a08ecfd4cb3de25e0cf8715cfcdcfcd21d250f3b
souzaiarley <souzaiarley20@gmail.com>
2025-02-16 13:52:55 -0300

    tournament selection added

--- selection.py
+++ selection.py
@@ -1,7 +1,8 @@
 from random import randint
 
-def ranking(populationSize, population):
-    # Sort the population by fitness from highest to lowest
+# Rank-based selection
+def rankingSelection(populationSize, population):
+    # Sort the population by cost from highest to lowest
     population.sort(key=lambda x: x.fitness, reverse=True)
 
     for i in population:
@@ -20,6 +21,33 @@ def ranking(populationSize, population):
     for i in range(0, populationSize):
         parent1 = population[roulette[randint(0, len(roulette)-1)]]
         parent2 = population[roulette[randint(0, len(roulette)-1)]]
+
+        parents.append((parent1, parent2))
+
+    return parents
+
+# Tournament selection
+def executeTournament(population):
+    tournament = []
+    k = 3
+
+    # Select k individuals randomly from the population
+    for i in range(0, k):
+        tournament.append(population[randint(0, len(population)-1)])
+
+    # Sort the tournament by cost from lowest to highest
+    tournament.sort(key=lambda x: x.fitness)
+
+    # Return the individual with the lowest cost
+    return tournament[0]
+
+def tournamentSelection(populationSize, population):
+    parents = []
+
+    # Select the parents based on the tournament selection
+    for i in range(0, populationSize):
+        parent1 = executeTournament(population)
+        parent2 = executeTournament(population)
 
         parents.append((parent1, parent2))
 
