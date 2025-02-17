9143d640cd9a68207a3ba13538425a062764fd3c
souzaiarley <souzaiarley20@gmail.com>
2025-02-17 19:21:23 -0300

    mutation method added: Exchange Mutation (EM)

--- mutation.py
+++ mutation.py
@@ -1,6 +1,22 @@
 from random import randint
 from random import random
 
+# Function that implements the exchange mutation (EM)
+def exchange(offspring, mutationRate):
+    for child in offspring:
+        # Check if the mutation will be applied to the child
+        if random() < mutationRate:
+            # Select two random positions i and j in the permutation
+            i = randint(0, len(child) - 1)
+            j = randint(0, len(child) - 1)
+            while i == j:
+                j = randint(0, len(child) - 1)
+
+            # Exchange the elements in positions i and j
+            child[i], child[j] = child[j], child[i]
+
+    return offspring
+
 # Function that implements the simple inversion mutation (SIM)
 def inversion(offspring, mutationRate):
     for child in offspring:
