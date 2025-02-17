3aa1531ab008a1411b4ed18a5f52e7d3ead5a54f
souzaiarley <souzaiarley20@gmail.com>
2025-02-17 19:00:42 -0300

    mutation method added: Simple Inversion (SIM)

---
+++ mutation.py
@@ -0,0 +1,17 @@
+from random import randint
+from random import random
+
+# Function that implements the simple inversion mutation (SIM)
+def inversion(offspring, mutationRate):
+    for child in offspring:
+        # Check if the mutation will be applied to the child
+        if random() < mutationRate:
+            # Select two random positions i and j in the permutation
+            i = randint(0, len(child) - 2)
+            j = randint(i + 1, len(child) - 1)
+
+            # Invert the order of the elements between positions i and j
+            for k in range(0, (j - i + 1) // 2):
+                child[i + k], child[j - k] = child[j - k], child[i + k]
+
+    return offspring
