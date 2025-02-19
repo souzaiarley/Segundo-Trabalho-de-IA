a55095d6202bd1ee0d6740bd000875552c68f32e
souzaiarley <souzaiarley20@gmail.com>
2025-02-18 21:05:42 -0300

    elitismo balanceado adicionado

--- geneticMethods/elitism.py
+++ geneticMethods/elitism.py
@@ -1,6 +1,6 @@
 from math import ceil
 
-def elitism(population, populationSize, eliteRate):
+def pureElitism(population, populationSize, eliteRate):
     # Calculate the number of elite individuals
     eliteSize = ceil(populationSize * eliteRate)
 
@@ -11,3 +11,18 @@ def elitism(population, populationSize, eliteRate):
     elite = population[:eliteSize]
 
     return elite
+
+def balancedElitism(population, populationSize, eliteRate):
+    # Calculate the number of elite individuals
+    eliteSize = ceil(populationSize * eliteRate)
+
+    # Sort the population by cost from lowest to highest
+    population.sort(key=lambda x: x.fitness)
+
+    # Select the elite individuals
+    elite = population[:eliteSize]
+
+    # Select the worst individuals
+    elite = elite[::-1]
+    worstElite = elite[:eliteSize//2]
+    return worstElite
